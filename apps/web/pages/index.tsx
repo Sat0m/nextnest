388095e16391acd6314ef6da8b4a61a920246daf
Tomas Benda <bendatomastb@gmail.com>
2023-03-15 09:18:36 +0100

    first commit

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { Button } from "ui";
 
 export default function Web() {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    fetch("/api/")
+      .then((res) => {
+        console.log("res: ", res);
+        return res.text();
+      })
+      .then(setGreeting);
+  }, []);
+
   return (
     <div>
-      <h1>Web</h1>
+      <h1>{greeting || "ahoj"}</h1>
       <Button />
     </div>
   );
